dbd69fd4da402988ea8e3d2c7ddc23434dee0db9
instantpun <instantpun@gmail.com>
2018-12-29 05:40:05 -0600

    Delete basics_user-input.py

--- basics_user-input.py
+++
@@ -1,8 +0,0 @@
-## python 3, lesson 1
-# Basics - User Input
-
-user_says = input("please enter the string you want to print: \n");
-# input() is a built-in method that accepts a string as input from the terminal
-# a string can be passed to it, so a preface message can be printed to terminal
-
-print(user_says);
